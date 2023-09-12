32c64fac47abe940b110eeedf29972f7a112cfa4
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2023-09-12 10:28:21 +0200

    Update middleware.py

--- django_token/middleware.py
+++ django_token/middleware.py
@@ -1,7 +1,7 @@
 from django import http
-from django.contrib import auth
+from django.contrib.auth import login
 from django.core import exceptions
-
+from .backends import TokenBackend
 
 class TokenMiddleware(object):
     """
@@ -14,10 +14,17 @@ class TokenMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request):
+        #self.get_response = get_response
+
+        #if not self.get_response:
+        #    return exceptions.ImproperlyConfigured(
+        #        'Middleware called without proper initialization')
         self.process_request(request)
         return self.get_response(request)
 
     def process_request(self, request):
+        #self.get_response = get_response
+
         auth_header = str(request.META.get('HTTP_AUTHORIZATION', '')).partition(' ')
 
         if auth_header[0].lower() != 'token':
@@ -26,9 +33,10 @@ class TokenMiddleware(object):
         # If they specified an invalid token, let them know.
         if not auth_header[2]:
             return http.HttpResponseBadRequest("Improperly formatted token")
-
-        user = auth.authenticate(token=auth_header[2])
+        
+        user = TokenBackend().authenticate(token=auth_header[2])
         print(f"token: {user}")
+        login(request, user, backend="django.contrib.auth.backends.ModelBackend")
         
         if user:
             request.user = user
